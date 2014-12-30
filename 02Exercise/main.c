14fb70412dc051f7c89e0d23f816162f388202b8
Mark Nitz <daniellenitz@vanguard.local>
2014-12-30 11:35:53 +0200

    Initial Commit

---
+++ 02Exercise/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  02Exercise
+//
+//  Created by Mark Nitz on 12/30/14.
+//  Copyright (c) 2014 Mark Nitz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
